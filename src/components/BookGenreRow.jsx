e56bb6d6842690088bde47c665ba897b7e208b89
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-09-05 19:02:52 +0200

    fixes cards

--- src/components/BookGenreRow.jsx
+++ src/components/BookGenreRow.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 class BookGenreRow extends Component {
   render() {
@@ -10,7 +10,12 @@ class BookGenreRow extends Component {
           {this.props.books.map(book => (
             <Col key={`${book.asin}`} className="d-flex">
               <Card>
-                <Card.Img src={book.img} className="h-100 object-fit-contain" />
+                <Card.Img src={book.img} variant="top" style={{ height: "200px" }} className="object-fit-contain" />
+                <Card.Body>
+                  <Card.Title className="fs-6">{book.title}</Card.Title>
+                  <Card.Text>`${book.price}`</Card.Text>
+                  <Button variant="primary">Add to cart</Button>
+                </Card.Body>
               </Card>
             </Col>
           ))}
