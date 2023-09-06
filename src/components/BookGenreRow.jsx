99205a5cd7c71a93e5946d4da23614e927fa6e87
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-09-06 13:32:35 +0200

    temp

--- src/components/BookGenreRow.jsx
+++ src/components/BookGenreRow.jsx
@@ -1,46 +1,51 @@
 import { Component } from "react";
 import { Button, Card, Col, Row } from "react-bootstrap";
 
-let expanded = false;
-let colNum = 6;
+// let expanded = false;
+let colNumVar = 6;
 
 const showMoreHide = e => {
-  if (expanded) {
-    const row = e.target.closest("section").querySelector(".row");
-    const cols = row.querySelectorAll(".col");
-    const colsLen = cols.length;
-    for (let i = colNum; i < colsLen; i++) {
-      cols[i].classList.add("d-none");
-    }
+  if (expanded && e.target.classList.contains("more")) {
+    this.setState({ display: "d-none" });
     e.target.innerText = "More";
     expanded = false;
-  } else {
-    const section = e.target.closest("section");
-    const hiddenCols = section.querySelectorAll(".col.d-none");
-    hiddenCols.forEach(col => {
-      col.classList.remove("d-none");
-    });
+  } else if (!expanded) {
+    this.setState({ display: "d-block" });
     e.target.innerText = "Hide";
     expanded = true;
   }
 };
 class BookGenreRow extends Component {
   state = {
-    cols: colNum,
+    display: "d-block",
+    expanded: false,
+    showHideBtnText: "More",
   };
   render() {
     return (
       <section>
         <div className="d-flex justify-content-between align-items-center mb-4">
           <h3 className=" h2 m-0">{this.props.genre}</h3>
-          <Button variant="primary" onClick={showMoreHide}>
+          <Button
+            variant="primary"
+            onClick={() => {
+              if (this.state.expanded && e.target.classList.contains("more")) {
+                this.setState({ display: "d-none" });
+                this.setState({ showHideBtnText: "Hide" });
+                this.setState({ expanded: false });
+              } else if (!this.state.expanded) {
+                this.setState({ display: "d-block" });
+                this.setState({ showHideBtnText: "More" });
+                this.setState({ expanded: true });
+              }
+            }}
+          >
             More
           </Button>
         </div>
         <Row className="row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 gy-4 mb-5">
           {this.props.books.map((book, index) => (
-            <Col key={`${book.asin}`} className={`d-flex ${index < this.state.cols ? null : "d-none"}`}>
-              {/* // <Col key={`${book.asin}`} className="d-flex"> */}
+            <Col key={`${book.asin}`} className={`${this.state.display} ${index < colNumVar ? null : "d-none more"}`}>
               <Card>
                 <Card.Img src={book.img} variant="top" style={{ height: "200px" }} className="object-fit-contain" />
                 <Card.Body>
